2fbcbc3f1dbfdf362782c3afd0009b01c86b8773
kylerisse <kylerisse@users.noreply.github.com>
2022-03-19 22:28:10 -0700

    add yamllint and bump python to 3.10 in nix shell

--- shell.nix
+++ shell.nix
@@ -4,12 +4,12 @@
 with pkgs;
 
 let
-  scale_python = python38.withPackages
-      (pythonPackages: with pythonPackages; [ pytest pylint ]);
+  scale_python = python310.withPackages
+      (pythonPackages: with pythonPackages; [ pytest pylint yamllint ]);
 
   # Trying to keep these pkg sets separate for later
   global = [ bash curl git jq kermit  screen glibcLocales ] ++ [ scale_python ];
-  ansible_sub = [ansible_2_11 ansible-lint];
+  ansible_sub = [ ansible_2_11 ansible-lint ];
   openwrt_sub = [ expect gomplate magic-wormhole tftp-hpa ];
   network_sub = [ perl532 ];
 in
